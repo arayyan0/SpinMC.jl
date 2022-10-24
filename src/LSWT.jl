e49ea670e284382d8b645a1060edf7501d5e5875
arayyan0 <a.rayyan@mail.utoronto.ca>
2022-10-24 15:14:55 -0400

    including LSWT.jl; prepared function to find local rotation given a spin

---
+++ src/LSWT.jl
@@ -0,0 +1,13 @@
+using LinearAlgebra
+
+struct LSWT
+end
+
+function findLocalRotation(spin::Vector{Float64})::Matrix{Float64}
+    # given spin = (s1, s2, s3) with s1,s2,s3 real, return matrix R such that
+    #                     (s1, s2, s3).T = R (0, 0, 1).T
+    # then s3 is the quantization axis with s1, s2 spanning the perpendicular plane
+    s1, s2, s3 = spin
+    sperp = norm(spin[1:2])
+    return sperp ≈ 0 ? I : [s3*s1/sperp s3*s2/sperp -sperp; -s2/sperp s1/sperp 0; s1 s2 s3]' 
+end
